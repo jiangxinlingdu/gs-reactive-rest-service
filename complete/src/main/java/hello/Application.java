82f1605e929e31e8eefc82673874e713b5c9edf9
lingdu <xxxx@qq.com>
2019-12-03 11:27:03 +0800

    修改例子，让变得更简单

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -8,8 +8,5 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
-		GreetingWebClient gwc = new GreetingWebClient();
-		System.out.println(gwc.getResult());
 	}
 }
